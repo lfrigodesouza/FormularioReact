d867741eb20d944bd5f05292a0a4e30806860bf5
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2020-08-12 17:05:21 -0300

    Criação do formulário

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
+
+function FormularioCadastro() {
+  return (
+    <form>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
+
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
+}
+
+export default FormularioCadastro;
